f124f7a17bcb0f2fc39f4ae95f55714cca4889af
Artem <artem.sopizhenko@quick.no>
2025-07-19 09:35:02 +0200

    - implemented api

---
+++ README.md
@@ -0,0 +1,13 @@
+# Домашнє завдання. Тема 4. REST API
+
+## API Endpoints
+### Base URL: `http://localhost:3000/api/contacts`
+
+| Method | Endpoint            | Description        |
+| ------ | ------------------- | ------------------ |
+| GET    | `/api/contacts`     | Get all contacts   |
+| GET    | `/api/contacts/:id` | Get contact by ID  |
+| POST   | `/api/contacts`     | Create new contact |
+| PUT    | `/api/contacts/:id` | Update contact     |
+| DELETE | `/api/contacts/:id` | Delete contact     |
+
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,28 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().required().messages({
+    "string.empty": "Name cannot be empty",
+    "any.required": "Name is required",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.email": "Email must be a valid email address",
+    "any.required": "Email is required",
+  }),
+  phone: Joi.string().required().messages({
+    "string.empty": "Phone cannot be empty",
+    "any.required": "Phone is required",
+  }),
+});
 
 export const updateContactSchema = Joi.object({
-
+  name: Joi.string().optional(),
+  email: Joi.string().email().optional().messages({
+    "string.email": "Email must be a valid email address",
+  }),
+  phone: Joi.string().optional(),
 })
+  .min(1)
+  .messages({
+    "object.min": "Body must have at least one field",
+  });
