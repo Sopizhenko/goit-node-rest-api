6919d74d2f58bc69baac33c6c871cd9f210acea8
Artem <artem.sopizhenko@quick.no>
2025-07-21 12:45:35 +0200

    - fixed user model

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,11 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  logout,
+  currentUser,
+} from "../controllers/authController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { currentUser } from "../controllers/currentUserController.js";
 
 const router = express.Router();
 
